03b7c1b9be515ebc6bd08ea612b57066a2d258b1
Carli Carter <Carli Carter>
2017-09-08 11:41:50 -0400

    Add project files.

---
+++ CS245 - Lab 2 - Cipher/decrypt.h
@@ -0,0 +1,12 @@
+#ifndef DECRYPT_H
+#define DECRYPT_H
+
+#include "stdafx.h"
+#include <string>
+
+// Function declaration:z
+// decrypt(string &cipher_text, unsigned key)
+
+string decrypt(string &, unsigned);
+
+#endif
